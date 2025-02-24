83810984eec51cb2614082e6d6f16c355f3b5da8
HwangRock <157935545+HwangRock@users.noreply.github.com>
2025-02-25 03:07:41 +0900

    feat: 사용자 요청 DTO

---
+++ src/main/java/com/example/authserver/dto/request/LoginRequestDTO.java
@@ -0,0 +1,15 @@
+package com.example.authserver.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequestDTO {
+    String email;
+    String password;
+}
